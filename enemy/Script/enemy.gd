b6508cb40c17b04ae3e73a5a8128c5b3a769c0a2
erreur_130 <dosetloic@gmail.com>
2025-11-08 20:25:23 +0100

    fourmi colision

--- enemy/Script/enemy.gd
+++ enemy/Script/enemy.gd
@@ -5,11 +5,14 @@ class_name Enemy
 @export var horizontal_speed = 30
 @export var vertical_speed = 100
 
+var velocity: Vector2
+
 
 @onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
 
 func _process(delta):
-	position.x -= delta * horizontal_speed
+	velocity.x = delta * - horizontal_speed
+	move_and_collide(velocity)
 	
 func die():
 	horizontal_speed = 0

a473bda3797e74232c22666639e2c4cf6650b220
Loïc <dosetloic@gmail.com>
2025-11-08 17:11:43 +0100

    modif fourmi/ colision layer

--- enemy/Script/enemy.gd
+++ enemy/Script/enemy.gd
@@ -1,18 +1,15 @@
-extends Area2D
+extends RigidBody2D
 
 class_name Enemy
 
 @export var horizontal_speed = 30
 @export var vertical_speed = 100
 
-@onready var ray_cast_2d = $RayCast2D as RayCast2D
+
 @onready var animated_sprite_2d = $AnimatedSprite2D as AnimatedSprite2D
 
 func _process(delta):
 	position.x -= delta * horizontal_speed
-	
-	if !ray_cast_2d.is_colliding():
-		position.y += delta * vertical_speed
 	
 func die():
 	horizontal_speed = 0
